0d32f100f2fdff5b57527ba20b1b9636f44be5b8
Christian Haas <christian.haas@sevensuns.at>
2015-03-18 07:23:34 +0100

    added MemoryBlockHolder

---
+++ chunk/MemoryBlockHolder_test.go
@@ -0,0 +1,40 @@
+package chunk
+
+import (
+	"github.com/inkyblackness/res"
+
+	check "gopkg.in/check.v1"
+)
+
+type MemoryBlockHolderSuite struct {
+}
+
+var _ = check.Suite(&MemoryBlockHolderSuite{})
+
+func (suite *MemoryBlockHolderSuite) SetUpTest(c *check.C) {
+
+}
+
+func (suite *MemoryBlockHolderSuite) TestChunkTypeReturnsProvidedValue(c *check.C) {
+	holder := NewBlockHolder(BasicChunkType.WithCompression(), res.Data, [][]byte{})
+
+	c.Assert(holder.ChunkType(), check.Equals, BasicChunkType.WithCompression())
+}
+
+func (suite *MemoryBlockHolderSuite) TestContentTypeReturnsProvidedValue(c *check.C) {
+	holder := NewBlockHolder(BasicChunkType.WithCompression(), res.Data, [][]byte{})
+
+	c.Assert(holder.ContentType(), check.Equals, res.Data)
+}
+
+func (suite *MemoryBlockHolderSuite) TestBlockCountReturnsNumberOfBlocks(c *check.C) {
+	holder := NewBlockHolder(BasicChunkType.WithCompression(), res.Data, [][]byte{[]byte{0x01}, []byte{0x02}})
+
+	c.Assert(holder.BlockCount(), check.Equals, uint16(2))
+}
+
+func (suite *MemoryBlockHolderSuite) TestBlockDataReturnsData(c *check.C) {
+	holder := NewBlockHolder(BasicChunkType.WithCompression(), res.Data, [][]byte{[]byte{0x01}, []byte{0x02}})
+
+	c.Assert(holder.BlockData(1), check.DeepEquals, []byte{0x02})
+}
