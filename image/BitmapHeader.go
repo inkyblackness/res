fcb9300a08fec2355c785280d552b1cc18b73f02
Christian Haas <christian.haas@sevensuns.at>
2015-06-10 20:33:54 +0200

    Named palette offset field - added String() function

--- image/BitmapHeader.go
+++ image/BitmapHeader.go
@@ -1,5 +1,9 @@
 package image
 
+import (
+	"fmt"
+)
+
 // BitmapHeader describes the header of an encoded bitmap
 type BitmapHeader struct {
 	Unknown0000 [4]byte
@@ -13,5 +17,12 @@ type BitmapHeader struct {
 	HeightFactor byte
 	HotspotBox   [4]int16
 
-	Unknown0018 int32
+	PaletteOffset int32
+}
+
+func (header *BitmapHeader) String() (result string) {
+	result += fmt.Sprintf("Type: 0x%02X, %dx%d\n", header.Type, header.Width, header.Height)
+	result += fmt.Sprintf("0006: 0x%04X\n", header.Unknown0006)
+	result += fmt.Sprintf("%d,%d | %d,%d\n", header.HotspotBox[0], header.HotspotBox[1], header.HotspotBox[2], header.HotspotBox[3])
+	return
 }
