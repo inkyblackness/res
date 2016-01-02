e51fe78db684fde1d6a43d43df134d80290534da
Christian Haas <christian.haas@sevensuns.at>
2016-01-02 09:10:29 +0100

    Fixed values for geometry are 32/16, not 32/8

--- geometry/command/Fixed.go
+++ geometry/command/Fixed.go
@@ -9,11 +9,14 @@ import (
 // Fixed is a value type for serialized coordinates.
 type Fixed uint32
 
+const fixedFactor = float32(0x10000)
+
 // ToFixed creates a Fixed out of a floating point value.
 func ToFixed(value float32) Fixed {
-	return Fixed(uint32(value * 256.0))
+	return Fixed(uint32(value * fixedFactor))
 }
 
+// CodeFixed serializes a Fixed value with a coder.
 func CodeFixed(coder serial.Coder, fixed *Fixed) {
 	raw := uint32(*fixed)
 	coder.CodeUint32(&raw)
@@ -22,7 +25,7 @@ func CodeFixed(coder serial.Coder, fixed *Fixed) {
 
 // Float returns the closest floating point value to the fixed value.
 func (fixed Fixed) Float() float32 {
-	return float32(int32(fixed)) / 256.0
+	return float32(int32(fixed)) / fixedFactor
 }
 
 // String returns the string presentation of the result of Float().
