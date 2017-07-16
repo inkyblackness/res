0811100096be21879e6f5a1759533c34885710d7
Christian Haas <christian.haas@sevensuns.at>
2017-07-16 15:48:57 +0200

    added formatter to ranged values

--- data/levelobj/Barriers.go
+++ data/levelobj/Barriers.go
@@ -1,6 +1,8 @@
 package levelobj
 
 import (
+	"fmt"
+
 	"github.com/inkyblackness/res/data/interpreters"
 )
 
@@ -9,7 +11,10 @@ var baseBarrier = interpreters.New().
 	With("LockMessageIndex", 2, 1).
 	With("ForceDoorColor", 3, 1).
 	With("RequiredAccessLevel", 4, 1).
-	With("AutoCloseTime", 5, 1).
+	With("AutoCloseTime", 5, 1).As(interpreters.FormattedRangedValue(0, 255,
+	func(value int64) string {
+		return fmt.Sprintf("%.2f sec  - raw: %d", float64(value)*0.5, value)
+	})).
 	With("OtherObjectIndex", 6, 2).As(interpreters.ObjectIndex())
 
 func initBarriers() interpreterRetriever {
