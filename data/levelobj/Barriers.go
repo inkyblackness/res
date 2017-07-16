b9032cbbc6fa1da4cd9c53608336e3a7d3de1138
Christian Haas <christian.haas@sevensuns.at>
2017-07-16 21:52:55 +0200

    more details on various properties

--- data/levelobj/Barriers.go
+++ data/levelobj/Barriers.go
@@ -9,7 +9,15 @@ import (
 var baseBarrier = interpreters.New().
 	With("LockVariableIndex", 0, 2).As(interpreters.RangedValue(0, 0x1FF)).
 	With("LockMessageIndex", 2, 1).
-	With("ForceDoorColor", 3, 1).
+	With("ForceDoorColor", 3, 1).As(interpreters.FormattedRangedValue(0, 255,
+	func(value int64) (result string) {
+		if colorText, defined := forceColors[value]; defined {
+			result = fmt.Sprintf("%s  - raw: %d", colorText, value)
+		} else {
+			result = fmt.Sprintf("%d", value)
+		}
+		return result
+	})).
 	With("RequiredAccessLevel", 4, 1).
 	With("AutoCloseTime", 5, 1).As(interpreters.FormattedRangedValue(0, 255,
 	func(value int64) string {
