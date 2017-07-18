81cb6f0aa3f3e9b62eaf49763da123ee17acc119
Christian Haas <christian.haas@sevensuns.at>
2017-07-18 07:09:44 +0200

    door access level bound to access mask names

--- data/levelobj/Barriers.go
+++ data/levelobj/Barriers.go
@@ -18,7 +18,15 @@ var baseBarrier = interpreters.New().
 		}
 		return result
 	})).
-	With("RequiredAccessLevel", 4, 1).
+	With("RequiredAccessLevel", 4, 1).As(interpreters.FormattedRangedValue(0, 30,
+	func(value int64) (result string) {
+		if accessLevel, known := accessLevelMasks[1<<uint32(value)]; known {
+			result = fmt.Sprintf("%s  - raw: %d", accessLevel, value)
+		} else {
+			result = fmt.Sprintf("Unknown  - raw: %d", value)
+		}
+		return
+	})).
 	With("AutoCloseTime", 5, 1).As(interpreters.FormattedRangedValue(0, 255,
 	func(value int64) string {
 		return fmt.Sprintf("%.2f sec  - raw: %d", float64(value)*0.5, value)
